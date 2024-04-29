bfd62afb1000f691a0cfdc345d13f30ed9bc040e
Adam-And-Alex <hoop.explorer@gmail.com>
2024-04-28 20:26:53 -0400

    Add collision boundary to zombie

--- player.gd
+++ player.gd
@@ -1,11 +1,12 @@
-extends Area2D
+extends RigidBody2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$AnimatedSprite2D.play("front")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#TODO: get velocity based on direcion of player
+	#TODO: make sprite depend on direction of travel
 	pass
